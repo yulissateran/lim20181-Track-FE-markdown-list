4089e3b93f6bd67c0254d356b318926077b12024
yulissateran <yulissa.lteran@gmail.com>
2018-08-12 14:29:45 -0500

    detector de eventos en archivos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 const fs = require('fs');
 
-fs.open('../README.md', 'r', (err, fd) => {
-  if (err) throw err;
-  fs.fstat(fd, (err, stat) => {
-    console.log(stat);
-    if (err) throw err;
-    // use stat
-
-    // always close the file descriptor!
-    fs.close(fd, (err) => {
-      if (err) throw err;
-    });
-  });
+fs.watch('./main.html', { encoding: 'utf8' }, (eventType, filename) => {
+  console.log( eventType);
+  if (filename) {
+    
+    // Prints: <Buffer ...>
+  }
 });
