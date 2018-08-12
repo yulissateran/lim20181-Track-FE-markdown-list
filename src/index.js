aed56060ec0243a16eba21ca19554b2e27281ced
yulissateran <yulissa.lteran@gmail.com>
2018-08-12 16:58:42 -0500

    list files  directory

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 
-fs.open('../README.md', 'r', (err, fd) => {
+fs.open('carpeta', 'r', (err, fd) => {
   if (err) throw err;
   fs.fstat(fd, (err, stat) => {
     if (err) throw err;
     // use stat
-    console.log(stat);
-    console.log(stat.isFile());
-    console.log(stat.isDirectory());
+    if (stat.isDirectory()) {
+      fs.readdir('carpeta', (err, files) => {
+        if (err) console.log(err);
+        files.forEach((file) => {
+          console.log(file)
+        }
+        );
+
+      });
+    }
     // always close the file descriptor!
     fs.close(fd, (err) => {
       if (err) throw err;
@@ -15,8 +22,3 @@ fs.open('../README.md', 'r', (err, fd) => {
   });
 });
 
-// destination.txt will be created or overwritten by default.
-fs.copyFile('./main.html', './index.html', (err) => {
-  if (err) throw err;
-  console.log('source.txt was copied to destination.txt');
-});
