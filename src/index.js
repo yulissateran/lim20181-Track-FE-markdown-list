bdac187566faa5e4f273fb28acdd27fa9066069a
yulissateran <yulissa.lteran@gmail.com>
2018-08-12 15:11:52 -0500

    copia texto de un archivo a otro

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 const fs = require('fs');
 
-// fs.open('../README.md', 'r', (err, fd) => {
-//   if (err) throw err;
-//   fs.fstat(fd, (err, stat) => {
-//     if (err) throw err;
-//     // use stat
-//     console.log(stat);
-//     console.log(stat.isFile());
-//     console.log(stat.isDirectory());
-//     // always close the file descriptor!
-//     fs.close(fd, (err) => {
-//       if (err) throw err;
-//     });
-//   });
-
-// });
+fs.open('../README.md', 'r', (err, fd) => {
+  if (err) throw err;
+  fs.fstat(fd, (err, stat) => {
+    if (err) throw err;
+    // use stat
+    console.log(stat);
+    console.log(stat.isFile());
+    console.log(stat.isDirectory());
+    // always close the file descriptor!
+    fs.close(fd, (err) => {
+      if (err) throw err;
+    });
+  });cd ..
+});
 
 // destination.txt will be created or overwritten by default.
 fs.copyFile('./main.html', './index.html', (err) => {
