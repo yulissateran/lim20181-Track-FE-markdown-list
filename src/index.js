9005898f17be728a748d6ce42dfa7877de17f7c3
yulissateran <yulissa.lteran@gmail.com>
2018-08-12 15:08:46 -0500

    diferencia archivo/carpeta

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
 
-fs.watch('./main.html', { encoding: 'utf8' }, (eventType, filename) => {
-  console.log( eventType);
-  if (filename) {
-    
-    // Prints: <Buffer ...>
-  }
+// fs.open('../README.md', 'r', (err, fd) => {
+//   if (err) throw err;
+//   fs.fstat(fd, (err, stat) => {
+//     if (err) throw err;
+//     // use stat
+//     console.log(stat);
+//     console.log(stat.isFile());
+//     console.log(stat.isDirectory());
+//     // always close the file descriptor!
+//     fs.close(fd, (err) => {
+//       if (err) throw err;
+//     });
+//   });
+
+// });
+
+// destination.txt will be created or overwritten by default.
+fs.copyFile('./main.html', './index.html', (err) => {
+  if (err) throw err;
+  console.log('source.txt was copied to destination.txt');
 });
